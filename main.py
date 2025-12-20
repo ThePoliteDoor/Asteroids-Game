c7a81fb1504bf3c2c39a4647efe85f608eac8539
ThePoliteDoor <oyashiro05@gmail.com>
2025-12-20 08:47:30 +0800

    A: Start of Asteriod Game

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+VERSION = pygame.version.ver
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+running = True
+
+while running:
+    log_state()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill("black")
+    pygame.display.flip()
+
+def main():
+    print(f"Starting Asteroids with pygame version: {VERSION}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
+    
