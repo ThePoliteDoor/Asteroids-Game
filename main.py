9d6d99b645779572b2877c002ac5a2e217d3b3a1
ThePoliteDoor <oyashiro05@gmail.com>
2025-12-20 10:39:16 +0800

    B: player, movement and rotation

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 VERSION = pygame.version.ver
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+fps = pygame.time.Clock()
+dt = 0
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
+player = Player(x, y)
+
 
 running = True
 
@@ -15,7 +22,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill("black")
+    player.update(dt)
+    player.draw(screen)
+    
     pygame.display.flip()
+
+    dt = fps.tick(60) / 1000
 
 def main():
     print(f"Starting Asteroids with pygame version: {VERSION}")
